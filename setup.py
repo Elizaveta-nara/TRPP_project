6276392fbde5cb270f322597245fddaefd23c1cf
faleeeerrr.a <valera.val2014@gmail.com>
2025-05-15 10:52:54 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auto',
+    version='1.0',
+    author='faleeeerrr.a',
+    author_email='valera.val2014@gmail.com',
+    packages=find_packages(),
+)
